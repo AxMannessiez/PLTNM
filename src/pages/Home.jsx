b853ee559f33ff977154742b0fcbef915872ad70
AxMannessiez <axelmannessiez@yahoo.fr>
2022-11-26 21:11:06 -0800

    Regroup Props for Music Services Logo Cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,21 +14,23 @@ import {Center, SimpleGrid} from "@chakra-ui/react";
 
 
 function Home(){
-    const cardW = '150px';
-    const cardNameMargin = 2;
+    const cardStyleProps = {
+        w: '150px',
+        nameMargin: 2,
+    }
     return (
         <>
             <Center mt={20}>
                 <SimpleGrid columns={3} spacingX={5} spacingY={4} maxW={'600px'}>
-                    <SpotifyLogoCard w={cardW} nameMargin={cardNameMargin}/>
-                    <AppleMusicLogoCard w={cardW} nameMargin={cardNameMargin}/>
-                    <DeezerLogoCard w={cardW} nameMargin={cardNameMargin}/>
-                    <YoutubeMusicLogoCard w={cardW} nameMargin={cardNameMargin}/>
-                    <TidalLogoCard w={cardW} nameMargin={cardNameMargin}/>
-                    <PandoraLogoCard w={cardW} nameMargin={cardNameMargin}/>
-                    <SoundcloudLogoCard w={cardW} nameMargin={cardNameMargin}/>
-                    <AmazonMusicLogoCard w={cardW} nameMargin={cardNameMargin}/>
-                    <NapsterLogoCard w={cardW} nameMargin={cardNameMargin}/>
+                    <SpotifyLogoCard {...cardStyleProps}/>
+                    <AppleMusicLogoCard {...cardStyleProps}/>
+                    <DeezerLogoCard {...cardStyleProps}/>
+                    <YoutubeMusicLogoCard {...cardStyleProps}/>
+                    <TidalLogoCard {...cardStyleProps}/>
+                    <PandoraLogoCard {...cardStyleProps}/>
+                    <SoundcloudLogoCard {...cardStyleProps}/>
+                    <AmazonMusicLogoCard {...cardStyleProps}/>
+                    <NapsterLogoCard {...cardStyleProps}/>
                 </SimpleGrid>
             </Center>
         </>
