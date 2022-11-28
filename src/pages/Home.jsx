87fa5869ca1a86538ab8cd02a858a6a6d1c2ce66
AxMannessiez <axelmannessiez@yahoo.fr>
2022-11-27 21:29:43 -0800

    🗑️ Clean imports

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,4 @@
 import React from "react";
-import {
-    AmazonMusicLogoCard,
-    AppleMusicLogoCard,
-    DeezerLogoCard,
-    NapsterLogoCard,
-    SpotifyLogoCard,
-    TidalLogoCard,
-    PandoraLogoCard,
-    YoutubeMusicLogoCard, SoundcloudLogoCard
-} from "../components/StartSteps/MusicServicesLogoCards";
-
-import {Center, SimpleGrid} from "@chakra-ui/react";
 
 
 function Home(){
