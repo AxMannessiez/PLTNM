9ca995d916a8035e91480ca14da04f195dd634d7
AxMannessiez <axelmannessiez@yahoo.fr>
2022-12-03 02:29:47 -0800

    💄 Change Theme Color

--- src/auth/supabaseClient.js
+++ src/auth/supabaseClient.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
-import env from 'react-dotenv'
+import env from 'react-dotenv';
 
 const supabaseUrl = env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = env.REACT_APP_SUPABASE_ANON_KEY;
