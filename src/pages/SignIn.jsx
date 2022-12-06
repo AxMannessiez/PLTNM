986711f555547cf7fd1562186ac1cb4da428fcff
AxMannessiez <axelmannessiez@yahoo.fr>
2022-12-05 21:50:16 -0800

    ✨ Ask and store User's Name

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,29 +1,71 @@
 import {supabase} from "../auth/supabaseClient";
 import {customTheme, containerStyle, buttonStyle} from "../auth/customAuthUITheme";
+import {PltnmButton} from "../components/base/PltnmButton";
+
 
 import {Auth} from '@supabase/auth-ui-react'
-import {VStack, Heading, Text, ScaleFade, Button} from "@chakra-ui/react";
+import {VStack, Heading, Text, ScaleFade, Input, FormControl, FormLabel, FormErrorMessage} from "@chakra-ui/react";
+import { Field, Form, Formik } from 'formik';
 import {useState, useEffect} from "react";
+import {useNavigate} from "react-router-dom";
 import env from "react-dotenv";
+import {capitalize} from "lodash/string";
 
+// TODO Change error color to pltnm
 // TODO Remove User info example
-// TODO Add logout in header
 // TODO Add state parameter check to set the redirectTo
 // TODO Recode it without the Supabase UI (to get Chakra components)
 // TODO Put Spotify last if user chose other app for playlist
 
+function validateFormName(value) {
+    let error;
+    if (!value) {
+        error = 'Name is required';
+    }
+    return error;
+}
+
 const Container = (props) => {
     const {user} = Auth.useUser();
-    if (user)
-        return (
-            <>
-                <Text>Hello {user.identities[0]?.identity_data?.name ?? ''}!</Text>
-                <Text>Signed in: {user.email ?? ''}</Text>
-                <Button block={"true"} bg='pltnm.primary' onClick={() => props.supabaseClient.auth.signOut()}>
-                    Sign out
-                </Button>
-            </>
-        );
+    const navigate = useNavigate();
+    if (user) {
+        if (user.app_metadata && user.app_metadata.provider) {
+            return (
+                <>
+                    <Text pt={5}>Signed in with {capitalize(user.app_metadata.provider)} ✔️</Text>
+                    <Text pt={5}>Now we only need your name!</Text>
+                    <Formik
+                        initialValues={{ name: '' }}
+                        onSubmit={(values, actions) => {
+                            setTimeout(() => {
+                                localStorage.setItem("user_name", values.name);
+                                actions.setSubmitting(false);
+                                navigate("/");
+                            }, 100);
+                        }}
+                    >
+                        {(props) => (
+                            <Form>
+                                <VStack spacing={6}>
+                                    <Field name='name' validate={validateFormName}>
+                                        {({ field, form }) => (
+                                            <FormControl isInvalid={form.errors.name && form.touched.name}>
+                                                <Input {...field} placeholder='John' />
+                                                <FormErrorMessage>{form.errors.name}</FormErrorMessage>
+                                            </FormControl>
+                                        )}
+                                    </Field>
+                                    <PltnmButton mt={4} isLoading={props.isSubmitting} type='submit'>Continue</PltnmButton>
+                                </VStack>
+                            </Form>
+                        )}
+                    </Formik>
+                </>
+            );
+        }
+        return props.children;
+    }
+
     return props.children;
 };
 
@@ -34,9 +76,9 @@ export default function SignIn() {
     return (
         <VStack h='100vh' justify='center'>
             <Heading as='h1' fontSize='2xl' fontWeight='700'>Sign In</Heading>
-            <Text>We need an account to store your songs!</Text>
-            <ScaleFade initialScale={0.95} in={mounted} duration={'1s'}>
-                <Container supabaseClient={supabase}>
+            <Container supabaseClient={supabase}>
+                <Text>We need an account to store your songs!</Text>
+                <ScaleFade initialScale={0.95} in={mounted} duration={'1s'}>
                     <Auth
                         supabaseClient={supabase}
                         socialLayout='vertical'
@@ -63,8 +105,8 @@ export default function SignIn() {
                             },
                         }}
                     />
-                </Container>
-            </ScaleFade>
+                </ScaleFade>
+            </Container>
         </VStack>
     );
 };
